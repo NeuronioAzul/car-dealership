653149350f9e2719e41106f5aadc7faacd7e0708
Mauro Rocha <mauro.rocha.t@gmail.com>
2025-07-23 02:57:38 -0300

    fix: update binary operator spacing rule in PHP CS Fixer configuration

--- vehicle-service/.php-cs-fixer.php
+++ vehicle-service/.php-cs-fixer.php
@@ -2,13 +2,13 @@
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR12'                      => true,                    // PSR-12 (padrão mais aceito)
-        'array_syntax'                => ['syntax' => 'short'], // Usa [] ao invés de array()
-        'binary_operator_spaces'      => ['default' => 'align_single_space'], // Alinha operadores
+        '@PSR12' => true,                    // PSR-12 (padrão mais aceito)
+        'array_syntax' => ['syntax' => 'short'], // Usa [] ao invés de array()
+        'binary_operator_spaces' => ['default' => 'single_space'], // Alinha operadores
         'blank_line_before_statement' => [
             'statements' => ['return', 'throw', 'if', 'switch', 'try'],
         ],
-        'no_unused_imports'    => true,         // Remove use's não usados
+        'no_unused_imports' => true,         // Remove use's não usados
         'no_extra_blank_lines' => [
             'tokens' => [
                 'extra',
@@ -19,13 +19,13 @@ return (new PhpCsFixer\Config())
                 'curly_brace_block',
             ],
         ],
-        'ordered_imports'             => ['sort_algorithm' => 'alpha'], // Ordena os imports
-        'single_quote'                => true,              // Usa aspas simples quando possível
+        'ordered_imports' => ['sort_algorithm' => 'alpha'], // Ordena os imports
+        'single_quote' => true,              // Usa aspas simples quando possível
         'trailing_comma_in_multiline' => ['elements' => ['arrays']], // Vírgula no final de arrays multilinha
-        'phpdoc_order'                => true,              // Ordena @param, @return, etc. no phpdoc
-        'phpdoc_trim'                 => true,               // Remove espaços em branco antes/apos docblocks
+        'phpdoc_order' => true,              // Ordena @param, @return, etc. no phpdoc
+        'phpdoc_trim' => true,               // Remove espaços em branco antes/apos docblocks
         'no_blank_lines_after_phpdoc' => true, // Não deixa linha em branco após phpdoc
-        'method_argument_space'       => [
+        'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
         ],
         'concat_space' => ['spacing' => 'one'], // Espaço ao concatenar strings
